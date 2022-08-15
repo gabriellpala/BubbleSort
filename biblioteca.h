4d7c44237a0a5d96464229c7d653065b6a3c402d
Gabriel Pala <73294128+gabriellpala@users.noreply.github.com>
2022-08-15 14:20:37 -0300

    Update biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -6,33 +6,30 @@ void troca(int *a, int *b){
 	temp = *a;
 	*a = *b;
 	*b=temp;
-}// fim troca
-//----------------------
+}
 void geraVet(int v[], int n){
 	int i;
 	srand(time(NULL));
 	for(i=0;i<n;i++)
 	   v[i]=rand()%50;
-}// fim geraV
-//-----------------------------
+}
 void printVetor(int v[],int n){
 	int i;
 	for(i=0;i<n;i++)
 	  printf("%d |",v[i]);
 	printf("\n");  
-}// fim printVetor
-//--------- Bubble Sort -----------------
+}
 void bsort(int v[], int n){
 	int i,j;
-	for(i=1;i<n;i++){ // fases
-		for(j=0;j<n-i;j++){ // comparações
+	for(i=1;i<n;i++){ 
+		for(j=0;j<n-i;j++){ 
 			if(v[j]>v[j+1]){
 				troca(&v[j],&v[j+1]);
-			}// fim if
-		}// fim for j
-	}// fim for i
+			}
+		}
+	}
 }
-//------------- Selection Sort ---------------
+
 int minIndex(int v[], int i, int n){
 	int j, imenor = i;
 	for(j=i+1;j<n;j++)
@@ -46,7 +43,7 @@ void ssort(int v[], int n){
 	for(i=0;i<n-1;i++){
 		indiceMenor = minIndex(v,i,n);
 		troca(&v[i], &v[indiceMenor]);
-	}// fim for
+	}
 }
-//----------------------------------------
+
 
